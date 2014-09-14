cc7a64d491ae3562299ebd3f1e343f8dc967ebf6
Sebastien Wagener <sebastien.wagener@gmail.com>
2014-09-14 21:26:57 +0200

    initial commit

---
+++ src/example/demo.cpp
@@ -0,0 +1,59 @@
+#include "generic_format/generic_format.hpp"
+
+#include <fstream>
+#include <fstream>
+
+namespace demo {
+
+struct Packet {
+    std::uint32_t source, target;
+    std::uint16_t port;
+};
+
+format::ast::adapted_struct<
+    Packet,
+    format::ast::accessor<Packet, std::uint32_t, &Packet::source, format::scalars::uint32_le_t>,
+    format::ast::accessor<Packet, std::uint32_t, &Packet::target, format::scalars::uint32_le_t>,
+    format::ast::accessor<Packet, std::uint16_t, &Packet::port, format::scalars::uint16_le_t>> Packet_format;
+
+}
+
+// TODO create macro like:
+//FORMAT_ADJUST_STRUCT(
+//    demo::Packet,
+//    (source, std::uint32_t, format::scalars::uint32_le_t)
+//    (target, std::uint32_t, format::scalars::uint32_le_t)
+//    (port,   std::uint16_t, format::scalars::uint16_le_t))
+
+std::ostream& operator<<(std::ostream& os, const demo::Packet & p) {
+    os << "Packet[source=" << p.source << ", target=" << p.target << ", port=" << p.port << "]";
+    return os;
+}
+
+int main() {
+    std::string fileName {"/tmp/foo.out" };
+
+    using namespace format::binary;
+    using namespace format::scalars;
+    using namespace std;
+    using namespace demo;
+
+    auto f = uint16_le << uint32_le;
+    {
+        ofstream os {fileName, std::ios_base::out | std::ios_base::binary};
+        std::tuple<std::uint16_t, std::uint32_t> v {42, 99};
+        write(f, os, v);
+
+        Packet packet { 1, 2, 3 };
+        write(Packet_format, os, packet);
+    }
+    {
+        ifstream is {fileName, std::ios_base::in | std::ios_base::binary};
+        std::tuple<std::uint16_t, std::uint32_t> v;
+        read(f, is, v);
+        std::cout << "read: " << std::get<0>(v) << " " << std::get<1>(v) << std::endl;
+        Packet packet;
+        read(Packet_format, is, packet);
+        std::cout << "read: " << packet << std::endl;
+    }
+}
---
+++ src/main/generic_format/generic_format.hpp
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <iostream>
+#include <tuple>
+#include <type_traits>
+
+namespace format {
+
+namespace tags {
+struct raw {};
+struct sequence {};
+struct adapted_struct {};
+}
+
+namespace ast {
+
+template<class TAG>
+struct base {
+    using tag = TAG;
+};
+
+template<class T>
+struct raw : base<tags::raw> {
+    using native_type = T;
+};
+
+template<class F1, class F2>
+struct sequence : base<tags::sequence> {
+    using left = F1;
+    using right = F2;
+    using native_type = std::tuple<typename F1::native_type, typename F2::native_type>;
+};
+
+// TODO move out of io package (into adaptor::structs namespace?)
+// TODO alternative syntax if we only have getters/setters?
+// TODO can we also nest accessors?
+// TODO do we need the type T?
+template<class C, class T, T C::* A, class S>
+struct accessor {
+    static constexpr auto attribute = A;
+    using serialized_type = S;
+};
+
+
+// TODO use a parameter pack here!
+template<class T, class... FIELDS>
+struct adapted_struct : base<tags::adapted_struct> {
+    using native_type = T;
+    using fields_tuple = std::tuple<FIELDS...>;
+};
+
+}
+
+namespace scalars {
+
+#define __FORMAT_PRIMITIVE_TYPE(name, type) struct name##_t : ast::raw<type> {}; name##_t name;
+__FORMAT_PRIMITIVE_TYPE(int8_le, std::int8_t)
+__FORMAT_PRIMITIVE_TYPE(int16_le, std::int16_t)
+__FORMAT_PRIMITIVE_TYPE(int32_le, std::int32_t)
+__FORMAT_PRIMITIVE_TYPE(int64_le, std::int64_t)
+__FORMAT_PRIMITIVE_TYPE(uint8_le, std::uint8_t)
+__FORMAT_PRIMITIVE_TYPE(uint16_le, std::uint16_t)
+__FORMAT_PRIMITIVE_TYPE(uint32_le, std::uint32_t)
+__FORMAT_PRIMITIVE_TYPE(uint64_le, std::uint64_t)
+}
+
+namespace binary {
+template<class TAG, class F>
+struct io {
+    void write(std::ostream &, const typename F::native_type &);
+    void read(std::istream &, typename F::native_type &);
+};
+
+template<class F>
+void write(std::ostream & os, const typename F::native_type & v) {
+    return io<typename F::tag, F>().write(os, v);
+}
+
+template<class F>
+void read(std::istream & is, typename F::native_type & v) {
+    return io<typename F::tag, F>().read(is, v);
+}
+
+template<class F>
+void write(F f, std::ostream & os, const typename F::native_type & v) {
+    write<F>(os, v);
+}
+
+template<class F>
+void read(F f, std::istream & is, typename F::native_type & v) {
+    read<F>(is, v);
+}
+
+template<class F>
+struct io<tags::raw, F> {
+
+    void write(std::ostream & os, const typename F::native_type & v) {
+        os.write(reinterpret_cast<const char*>(&v), sizeof(v));
+    }
+
+    void read(std::istream & is, typename F::native_type & v) {
+        is.read(reinterpret_cast<char*>(&v), sizeof(v));
+    }
+
+};
+
+template<class F>
+struct io<tags::sequence, F> {
+
+    void write(std::ostream & os, const typename F::native_type & v) {
+        binary::write<typename F::left>(os, std::get<0>(v));
+        binary::write<typename F::right>(os, std::get<1>(v));
+    }
+
+    void read(std::istream & is, typename F::native_type & v) {
+        binary::read<typename F::left>(is, std::get<0>(v));
+        binary::read<typename F::right>(is, std::get<1>(v));
+    }
+
+};
+
+template<class F>
+struct io<tags::adapted_struct, F> {
+
+    using C = typename F::native_type;
+    using fields_tuple = typename F::fields_tuple;
+    static constexpr std::size_t number_of_fields = std::tuple_size<fields_tuple>();
+    static constexpr fields_tuple* fields_tuple_ptr = nullptr; // TODO this is an ugly hack, but currently I don't see a way around
+
+    void write(std::ostream & os, const C & c) {
+        write_fields(os, c, fields_tuple_ptr);
+    }
+
+    void read(std::istream & is, C & c) {
+        read_fields(is, c, fields_tuple_ptr);
+    }
+
+private:
+
+    template<std::size_t I = 0, class... FIELDS>
+    inline typename std::enable_if<I == number_of_fields, void>::type
+    write_fields(std::ostream & os, const C & c, const std::tuple<FIELDS...>* ptr) {}
+
+    template<std::size_t I = 0, class... FIELDS>
+    inline typename std::enable_if<I < number_of_fields, void>::type
+    write_fields(std::ostream & os, const C & c, const std::tuple<FIELDS...> * ptr) {
+        write_field<typename std::tuple_element<I, fields_tuple>::type>(os, c);
+        write_fields<I + 1>(os, c, ptr);
+    }
+
+    template<std::size_t I = 0, class... FIELDS>
+    inline typename std::enable_if<I == number_of_fields, void>::type
+    read_fields(std::istream & is, C & c, const std::tuple<FIELDS...>* ptr) {}
+
+    template<std::size_t I = 0, class... FIELDS>
+    inline typename std::enable_if<I < number_of_fields, void>::type
+    read_fields(std::istream & is, C & c, const std::tuple<FIELDS...> * ptr) {
+        read_field<typename std::tuple_element<I, fields_tuple>::type>(is, c);
+        read_fields<I + 1>(is, c, ptr);
+    }
+
+    template<class FIELD>
+    void write_field(std::ostream & os, const typename F::native_type & c) {
+        using serialized_type = typename FIELD::serialized_type;
+        binary::write<serialized_type>(os, c.*(FIELD::attribute));
+    }
+
+    template<class FIELD>
+    void read_field(std::istream & is, typename F::native_type & c) {
+        using serialized_type = typename FIELD::serialized_type;
+        binary::read<serialized_type>(is, c.*(FIELD::attribute));
+    }
+};
+
+}
+}
+
+template<class F1, class F2>
+format::ast::sequence<F1, F2> operator<<(const F1 &, const F2 &) {
+    return format::ast::sequence<F1, F2>();
+}
+
