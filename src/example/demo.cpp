0b66cf8f001f72bde93f77974c7487915e731588
Sebastien Wagener <sebastien.wagener@gmail.com>
2014-09-16 20:46:00 +0200

    add BSL license

--- src/example/demo.cpp
+++ src/example/demo.cpp
@@ -1,3 +1,11 @@
+/**
+    @file
+    @copyright
+        Copyright Sebastien Wagener 2014
+        Distributed under the Boost Software License, Version 1.0.
+        (See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt)
+*/
 #include "generic_format/generic_format.hpp"
 
 #include <fstream>
