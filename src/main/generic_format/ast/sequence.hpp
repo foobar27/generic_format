a90e573b31253c5d4717414c5142f4b50c9ff93f
Sebastien Wagener <sebastien.wagener@gmail.com>
2014-12-24 17:39:40 +0100

    make ast::sequence variadic

--- src/main/generic_format/ast/sequence.hpp
+++ src/main/generic_format/ast/sequence.hpp
@@ -10,36 +10,61 @@
 
 #include "generic_format/ast/base.hpp"
 
+#include <tuple>
+
 namespace generic_format {
 namespace ast {
 
-/** @brief Two formats concatenated sequentially.
+/** @brief Zero or more formats concatenated sequentially.
  *
- * The native type for this format is a std::tuple of size 2.
+ * The native type for this format is a std::tuple, the size of which being the number of template arguments given to this struct.
  *
- * @tparam Format1 the format for the first element of the tuple
- * @tparam Format2 the format for the second element of the tuple
+ * @tparam Formats the formats of the different elements of the tuple
  */
-template<class Format1, class Format2>
-struct sequence : base<children_list<Format1, Format2>> {
-    static_assert(is_format<Format1>::value, "Format1 must be a format!");
-    static_assert(is_format<Format2>::value, "Format2 must be a format!");
+template<class... Formats>
+struct sequence : base<children_list<Formats...>> {
+    static_assert(variadic::for_all<is_format, Formats...>::value, "All components must be formats!");
 
-    using left = Format1;
-    using right = Format2;
-    using native_type = std::tuple<typename Format1::native_type, typename Format2::native_type>;
-    static constexpr auto size = Format1::size + Format2::size;
+    using native_type = std::tuple<typename Formats::native_type...>;
+    using elements_tuple = std::tuple<Formats...>;
+
+    static constexpr auto size = sum(fixed_size(0), Formats::size...);
+    static constexpr auto number_of_elements = sizeof...(Formats);
 
     template<class RawWriter, class State>
     void write(RawWriter & raw_writer, State & state, const native_type & t) const {
-        Format1().write(raw_writer, state, std::get<0>(t));
-        Format2().write(raw_writer, state, std::get<1>(t));
+        write_elements<RawWriter, State>(raw_writer, state, t);
     }
 
     template<class RawReader, class State>
     void read(RawReader & raw_reader, State & state, native_type & t) const {
-        Format1().read(raw_reader, state, std::get<0>(t));
-        Format2().read(raw_reader, state, std::get<1>(t));
+        read_elements<RawReader, State>(raw_reader, state, t);
+    }
+
+private:
+
+    template<class RawWriter, class State, std::size_t I = 0>
+    inline typename std::enable_if<I == number_of_elements, void>::type
+    write_elements(RawWriter &, State &, const native_type &) const {}
+
+    template<class RawWriter, class State, std::size_t I = 0>
+    inline typename std::enable_if<I < number_of_elements, void>::type
+    write_elements(RawWriter & raw_writer, State & state, const native_type & t) const {
+        using element_format = typename std::tuple_element<I, elements_tuple>::type;
+        element_format().write(raw_writer, state, std::get<I>(t));
+        write_elements<RawWriter, State, I + 1>(raw_writer, state, t);
+    }
+
+    template<class RawReader, class State, std::size_t I = 0>
+    inline typename std::enable_if<I == number_of_elements, void>::type
+    read_elements(RawReader &, State &, const native_type &) const {}
+
+    template<class RawReader, class State, std::size_t I = 0>
+    inline typename std::enable_if<I < number_of_elements, void>::type
+    read_elements(RawReader & raw_reader, State & state, native_type & t) const {
+        using element_format = typename std::tuple_element<I, elements_tuple>::type;
+        element_format().read(raw_reader, state, std::get<I>(t));
+        read_elements<RawReader, State, I + 1>(raw_reader, state, t);
     }
 };
 
